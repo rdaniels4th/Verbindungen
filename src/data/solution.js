04b83d1c4d4a297bae9f7bdac6d44e4c11d1aad1
rdaniels4th <redaniels4th@outlook.com>
2024-03-17 09:17:52 -0400

    update group names

--- src/data/solution.js
+++ src/data/solution.js
@@ -1,6 +1,6 @@
 export const solutions = {
-    "Innovation": ["einfach", ["Cipher", "Loom", "Harpsichord", "Juggernaut"]],
-    "Physics": ["mittel", ["Nebula", "Pendulum", "Dynamo", "Gyroscope"]],
+    "Medieval Innovation": ["einfach", ["Cipher", "Loom", "Harpsichord", "Juggernaut"]],
+    "Physics/scientific principles": ["mittel", ["Nebula", "Pendulum", "Dynamo", "Gyroscope"]],
     "Mythology": ["Tomate", ["Seraphim", "Albatross", "Narwhal", "Obsidian"]],
-    "Luminosity": ["unmöglich", ["Iridescence", "Kaleidoscope", "Lantern", "Maelstrom"]]
+    "Luminosity/light": ["unmöglich", ["Iridescence", "Kaleidoscope", "Lantern", "Maelstrom"]]
 }
